df1c8aad9d675c13a252783de30e6117e336d76c
Dora Choi <hellodoradora7@gmail.com>
2025-09-18 00:48:58 +0900

    chore & feat : add building models & add windowSpawner

---
+++ Assets/Scripts/WindowSpawner.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class WindowSpawner : MonoBehaviour
+{
+    public GameObject windowPrefab;
+
+    private int windowCount = 4;
+
+    private float xPos = -4.5f;
+    private float xOffset = 3f;
+    private float yPos = 4.7f;
+    private float yOffset = 2.5f;
+    private float zPos = -0.5f;
+
+    public GameObject[,] windows;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        windows = new GameObject[windowCount, windowCount];
+        SpawnWindow();
+    }
+
+    void SpawnWindow()
+    {
+        for (int i = 0; i < windowCount; i++)
+        {
+            for (int j = 0; j < windowCount; j++)
+            {
+                windows[i, j] = Instantiate(windowPrefab, new Vector3(xPos + i * xOffset, yPos - j * yOffset, zPos), Quaternion.identity);
+            }
+        }
+    }
+}
